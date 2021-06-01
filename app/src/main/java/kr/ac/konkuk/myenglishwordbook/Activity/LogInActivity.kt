f8a89a0551fe949b9fec9174017e90388ad1590b
easyhooon <51016231+easyhooon@users.noreply.github.com>
2021-06-01 17:30:40 +0900

    facebook login implementation

--- app/src/main/java/kr/ac/konkuk/myenglishwordbook/Activity/LogInActivity.kt
+++ app/src/main/java/kr/ac/konkuk/myenglishwordbook/Activity/LogInActivity.kt
@@ -2,9 +2,12 @@ package kr.ac.konkuk.myenglishwordbook.Activity
 
 import android.content.Intent
 import android.os.Bundle
-import android.util.Log
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import com.facebook.CallbackManager
+import com.facebook.FacebookCallback
+import com.facebook.FacebookException
+import com.facebook.login.LoginResult
 import com.google.android.gms.auth.api.Auth
 import com.google.android.gms.auth.api.signin.GoogleSignInAccount
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions
@@ -23,10 +26,15 @@ import kr.ac.konkuk.myenglishwordbook.R
 import kr.ac.konkuk.myenglishwordbook.databinding.ActivityLogInBinding
 
 
+
 class LogInActivity : AppCompatActivity(), GoogleApiClient.OnConnectionFailedListener {
 
     private var backBtnTime: Long = 0 // 뒤로가기 두번 눌러 종료 용 변수
     private lateinit var auth: FirebaseAuth
+
+    //페이스북 로그인버튼을 눌르면 페이스북 앱이 열림, 그리고 로그인 완료가 되면 다시 액티비티로 넘어옴(Activity Callback으로 넘어옴) -> onActivityResult 가 열림
+    // 페이스북에 갔다가 onActivityResult 에서 가져온 값을 다시 페이스북 sdk 에 전달해 줌으로써 실제로 로그인이 되었는지를 판단함
+    private lateinit var callbackManager: CallbackManager
 
     lateinit var binding: ActivityLogInBinding
 
@@ -42,9 +50,13 @@ class LogInActivity : AppCompatActivity(), GoogleApiClient.OnConnectionFailedLis
         //java에서 Firebase.getInstance()와 같이 Firebase Auth를 initialize 해주는 코드
         auth = Firebase.auth
         //초기화
+        callbackManager = CallbackManager.Factory.create()
+
         initLoginButton()
         initSignUpButton()
         initGoogleLoginButton()
+        initFacebookLoginButton()
+
     }
 
     private fun initLoginButton() {
@@ -104,6 +116,36 @@ class LogInActivity : AppCompatActivity(), GoogleApiClient.OnConnectionFailedLis
         }
     }
 
+    private fun initFacebookLoginButton() {
+        //유저에게 가져올 정보 -> 이메일과 프로필
+        binding.facebookLoginButton.setPermissions(EMAIL,PROFILE)
+        binding.facebookLoginButton.registerCallback(
+            callbackManager, object : FacebookCallback<LoginResult> {
+                override fun onSuccess(result: LoginResult) {
+                    // 로그인이 성공적 -> 로그인 엑세스 토큰을 가져온 다음에 파이어베이스에 넘김
+                    // 정상적으로 로그인 되었을 때는 result 로 null 이 내려오지 않을 것이기 때문에 result 에 ?을 제거
+                    val credential = FacebookAuthProvider.getCredential(result.accessToken.token)
+                    auth.signInWithCredential(credential)
+                        .addOnCompleteListener(this@LogInActivity) { task ->
+                            if (task.isSuccessful) {
+                                handleSuccessSocialLogin()
+                            } else {
+                                Toast.makeText(
+                                    this@LogInActivity,
+                                    FACEBOOK_LOGIN_FAIL, Toast.LENGTH_SHORT).show()
+                            }
+                        }
+                }
+                override fun onCancel() {}
+
+                override fun onError(error: FacebookException?) {
+                    //콜백함수안에 있기 때문에 @LogInActivity 에서 this 를 가져왔다고 명시를 해줘야함
+                    Toast.makeText(this@LogInActivity, FACEBOOK_LOGIN_FAIL, Toast.LENGTH_SHORT)
+                        .show()
+                }
+            })
+    }
+
 
     private fun handleSuccessSocialLogin() {
         if (auth.currentUser == null) {
@@ -115,7 +157,7 @@ class LogInActivity : AppCompatActivity(), GoogleApiClient.OnConnectionFailedLis
             val userName = auth.currentUser?.displayName.orEmpty()
             val userEmail = auth.currentUser?.email.orEmpty()
             val userProfileImage = auth.currentUser?.photoUrl.toString()
-            //reference가 최상위-> child child로 경로 지정
+            //reference 가 최상위-> child child로 경로 지정
             //경로가 존재하지 않으면 생성, 있으면 그 경로를 가져옴
             val userRef = Firebase.database.reference.child(USER).child(userId)
             val user = mutableMapOf<String, Any>()
@@ -136,10 +178,10 @@ class LogInActivity : AppCompatActivity(), GoogleApiClient.OnConnectionFailedLis
             startActivity(Intent(this, LogInActivity::class.java))
             return
         } else {
-            //currentUser는 nullable이기 때문에 위에 예외처리하였음
+            //currentUser 는 nullable 이기 때문에 위에 예외처리하였음
             val userReference = Firebase.database.reference.child(USER)
             val userId = auth.currentUser?.uid.orEmpty()
-            //reference가 최상위-> child child로 경로 지정
+            //reference 가 최상위-> child child 로 경로 지정
             //경로가 존재하지 않으면 생성, 있으면 그 경로를 가져옴
             val userRef = userReference.child(userId)
             val user = mutableMapOf<String, Any>()
@@ -169,6 +211,8 @@ class LogInActivity : AppCompatActivity(), GoogleApiClient.OnConnectionFailedLis
                     resultGoogleLogin(account)
                 }
             }
+        } else {
+            callbackManager.onActivityResult(requestCode, resultCode, data)
         }
     }
 
@@ -200,6 +244,8 @@ class LogInActivity : AppCompatActivity(), GoogleApiClient.OnConnectionFailedLis
     }
 
     companion object {
+        const val EMAIL = "email"
+        const val PROFILE = "public_profile"
         const val USER_NAME = "user_name"
         const val USER_EMAIL = "user_email"
         const val LOGIN_FAIL = "로그인에 실패했습니다. 이메일 또는 비밀번호를 확인해주세요."
@@ -207,6 +253,7 @@ class LogInActivity : AppCompatActivity(), GoogleApiClient.OnConnectionFailedLis
         const val ENTER_EMAIL = "이메일을 입력해주세요"
         const val ENTER_PASSWORD = "비밀번호를 입력해주세요"
         const val GOOGLE_LOGIN_FAIL = "구글 로그인에 실패했습니다"
+        const val FACEBOOK_LOGIN_FAIL = "페이스북 로그인에 실패했습니다"
         const val REG_SIGN_GOOGLE = 100
     }
 
